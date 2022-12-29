8bb8472c4c772b73d62d0205e2990f94bf0776ea
ross <2086516395@qq.com>
2022-12-30 01:50:21 +0800

    submit

---
+++ diagramitem.cpp
@@ -0,0 +1,130 @@
+#include "diagramitem.h"
+#include "arrow.h"
+
+#include <QGraphicsScene>
+#include <QGraphicsSceneContextMenuEvent>
+#include <QMenu>
+#include <QPainter>
+
+//! [0]
+DiagramItem::DiagramItem(DiagramType diagramType, QMenu *contextMenu,
+             QGraphicsItem *parent)
+    : QGraphicsPolygonItem(parent)
+{
+    myDiagramType = diagramType;
+    myContextMenu = contextMenu;
+
+    QPainterPath path;
+    switch (myDiagramType) {
+        case StartEnd:
+            path.moveTo(90, 0);
+            path.arcTo(40, -60, 50, 50, 0, 90);
+            path.arcTo(-80, -60, 50, 50, 90, 90);
+            path.arcTo(-80, 0, 50, 50, 180, 90);
+            path.arcTo(40, 0, 50, 50, 270, 90);
+          //  path.lineTo(90, 0);
+            myPolygon = path.toFillPolygon();
+            break;
+        case Conditional:
+            myPolygon << QPointF(-100, 0) << QPointF(0, 100)
+                      << QPointF(100, 0) << QPointF(0, -100)
+                      << QPointF(-100, 0);
+            break;
+        case Step:
+            myPolygon << QPointF(-100, -100) << QPointF(100, -100)
+                      << QPointF(100, 100) << QPointF(-100, 100)
+                      << QPointF(-100, -100);
+            break;
+        case Contact:
+            path.moveTo(50, 0);
+            path.arcTo(-50, -50, 100, 100, 0, 360);
+            //path.lineTo(90, 0);
+            myPolygon = path.toFillPolygon();
+            break;
+        case Documentation:
+            path.moveTo(50, 0);
+            path.arcTo(25, -50,50,50,90,0);
+            path.arcTo(-75, -50,50,50,90,0);
+            //path.arcTo(-50, 0,50,50,0,0);
+            path.arcTo(-50, -25,50,50,180,180);
+            path.arcTo(0, -25,50,50,0,180);
+           // path.lineTo(50, 0);
+            myPolygon = path.toFillPolygon();
+            break;
+        default:
+            myPolygon << QPointF(-120, -80) << QPointF(-70, 80)
+                      << QPointF(120, 80) << QPointF(70, -80)
+                      << QPointF(-120, -80);
+            break;
+    }
+    setPolygon(myPolygon);
+    setFlag(QGraphicsItem::ItemIsMovable, true);
+    setFlag(QGraphicsItem::ItemIsSelectable, true);
+    setFlag(QGraphicsItem::ItemSendsGeometryChanges, true);
+}
+//! [0]
+
+//! [1]
+void DiagramItem::removeArrow(Arrow *arrow)
+{
+    int index = arrows.indexOf(arrow);
+
+    if (index != -1)
+        arrows.removeAt(index);
+}
+//! [1]
+
+//! [2]
+void DiagramItem::removeArrows()
+{
+    foreach (Arrow *arrow, arrows) {
+        arrow->startItem()->removeArrow(arrow);
+        arrow->endItem()->removeArrow(arrow);
+        scene()->removeItem(arrow);
+        delete arrow;
+    }
+}
+//! [2]
+
+//! [3]
+void DiagramItem::addArrow(Arrow *arrow)
+{
+    arrows.append(arrow);
+}
+//! [3]
+
+//! [4]
+QPixmap DiagramItem::image() const
+{
+    QPixmap pixmap(250, 250);
+    pixmap.fill(Qt::transparent);
+    QPainter painter(&pixmap);
+    painter.setPen(QPen(Qt::black, 8));
+    painter.translate(125, 125);
+    painter.drawPolyline(myPolygon);
+
+    return pixmap;
+}
+//! [4]
+
+//! [5]
+void DiagramItem::contextMenuEvent(QGraphicsSceneContextMenuEvent *event)
+{
+    scene()->clearSelection();
+    setSelected(true);
+    myContextMenu->exec(event->screenPos());
+}
+//! [5]
+
+//! [6]
+QVariant DiagramItem::itemChange(GraphicsItemChange change, const QVariant &value)
+{
+    if (change == QGraphicsItem::ItemPositionChange) {
+        foreach (Arrow *arrow, arrows) {
+            arrow->updatePosition();
+        }
+    }
+
+    return value;
+}
+//! [6]
---
+++ diagramscene.pro
@@ -0,0 +1,18 @@
+QT += widgets
+requires(qtConfig(fontcombobox))
+
+HEADERS	    =   mainwindow.h \
+		diagramitem.h \
+		diagramscene.h \
+		arrow.h \
+		diagramtextitem.h
+SOURCES	    =   mainwindow.cpp \
+		diagramitem.cpp \
+		main.cpp \
+		arrow.cpp \
+		diagramtextitem.cpp \
+		diagramscene.cpp
+RESOURCES   =	diagramscene.qrc
+
+
+
